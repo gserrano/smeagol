14a1ef0f43c0f7cf6eaa3ef095eaa272c9a72714
Guilherme Serrano <guilhermeserrano@asiv.local>
2013-03-02 19:34:21 -0300

    Module configuration and basic crawling

--- example.js
+++ example.js
@@ -1,1 +1,25 @@
+require.main.paths.unshift('/usr/local/lib/node_modules/');
+
 var smeagol = require('smeagol');
+
+/* Create Smeagol configuration */
+smeagol.configure({
+    patterns : {
+        'http://andafter.org/publicacoes/([^/].)*?' : {
+            id : 'posts',
+            find : {
+            	id		: '("h1 a").attr("href")',
+            	name 	: '("h1 a").html()',
+            	olho 	: '(".olho").html()'
+            }
+        }
+    },
+    callback : function(result){
+        console.log(result);
+    }
+})
+
+/* Get info */
+smeagol.crawl({
+	uri : 'http://andafter.org/publicacoes/entrevista-de-emprego-unica.html'
+})
