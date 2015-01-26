586c53eca8aba5472e0db2ad12c907a0d952a810
gserrano <contato@guilhermeserrano.com.br>
2015-01-26 16:58:22 -0200

    No longer using jsdom, instead cheerio is now required (I think it's better to DOM navigation). Create a new configuration parameter: each_item (for now it is a required parameter)

--- example.js
+++ example.js
@@ -1,32 +1,37 @@
 require.main.paths.unshift('/usr/local/lib/node_modules/');
 
-var smeagol = require('smeagol'),
-	fs = require('fs');
+var smeagol = require('smeagol');
+
 
 /* Create Smeagol configuration */
 smeagol.configure({
-    patterns : { // Create patterns to URLs and content selectors to crawl
-        'http://eucompraria.com.br/produto/([^/].)*?' : {
-            id : 'posts',
+    crawl : [
+        {
+            pattern_url : '^http://eucompraria.com.br/produto/(.*)?$',
+            id : 'products',
+            each_item : '.product-item',
             find : {
-            	id		: '("h1 a").attr("href")',
-            	name 	: '("h1 a").html()',
-                price   : '(".item_price b").html()'
+                id      : '$(".product-title a").attr("href")',
+                title    : '$(".product-title").text()',
+                price    : '$(".item_price").attr("data-price")'
+            },
+            callback : function(result){
+                console.log(result);
+                return result;
             }
         }
-    },
-    limit: 10,
-    log : true, // Log smeagol-log.txt file
+    ],
+    log : true, // Log urls in smeagol-log.txt file
+    limit: 3, //max pages to crawl
     continuous : true, // Get all pages that url match pattern_to_crawl and automatic crawl this pages
     domain : 'http://eucompraria.com.br',
-    pattern_to_crawl : 'http://eucompraria.com.br/produto/([^/].)*?', // Continuous crawling will get url's that match this REGEX
-    callback : function(result){ // Execute when finish the crawl function
-        file = fs.createWriteStream('smeagol-result.txt', {'flags': 'a'});
-		file.write(JSON.stringify(result)+'\r\n');
+    pattern_to_crawl : '^http://eucompraria.com.br/produto/(.*)?$', 
+    callback : function(results){
+        console.log(results);
     }
 })
 
 /* Get info */
 var content = smeagol.crawl({
-	uri : 'http://eucompraria.com.br/'
+    uri : 'http://eucompraria.com.br/'
 });
